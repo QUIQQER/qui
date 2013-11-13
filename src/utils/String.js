73ab25fda2953fb7954c96ee2aa5b69bd07f7cef
Henning Leutz <leutz@pcsg.de>
2013-11-13 18:20:08 +0100

    form und string utils

---
+++ src/utils/String.js
@@ -0,0 +1,47 @@
+
+/**
+ * string helper
+ * Helps with string opartions
+ *
+ * @author www.pcsg.de (Henning Leutz)
+ */
+
+define('qui/utils/String', function()
+{
+    "use strict";
+
+    return {
+
+        /**
+         * get params from an url
+         *
+         * @method QUI.lib.Utils#getUrlParams
+         *
+         * @param {String} str - index.php?param1=12&param2=test
+         * @return {Object}
+         */
+        getUrlParams : function(str)
+        {
+            str = str.split('?');
+
+            if ( typeof str[1] === 'undefined' ){
+                return {};
+            }
+
+            str = str[1].split('&');
+
+            var i, len, sp;
+            var r = {};
+
+            for ( i = 0, len = str.length; i < len; i++ )
+            {
+                sp = str[i].split('=');
+
+                r[ sp[0] ] = sp[1];
+            }
+
+            return r;
+        }
+
+    };
+});
