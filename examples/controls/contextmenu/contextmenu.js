3fc3c902b4457f951c80b67319de58bdaa79a14c
Henning Leutz <leutz@pcsg.de>
2014-07-16 10:04:33 +0200

    context menu documentation

---
+++ examples/controls/contextmenu/contextmenu.js
@@ -0,0 +1,61 @@
+
+/**
+ * Example for a right click context menu
+ */
+
+require([
+
+    'qui/controls/contextmenu/Menu',
+    'qui/controls/contextmenu/Item'
+
+], function(QUIMenu, QUIMenuItem)
+{
+    "use strict";
+
+    // create the menu
+    var Menu = new QUIMenu({
+        events :
+        {
+            onBlur : function(Menu) {
+                Menu.hide();
+            }
+        }
+    }).inject( document.body );
+
+    Menu.appendChild(
+        new QUIMenuItem({
+            name : 'first',
+            text : 'First item',
+            icon : 'icon-home',
+            events :
+            {
+                onClick : function(Item) {
+                    alert( 'click: '+ Item.getAttribute( 'text' ) );
+                }
+            }
+        })
+    );
+
+    Menu.appendChild(
+        new QUIMenuItem({
+            name   : 'second',
+            text   : 'Second item',
+            icon   : 'icon-time',
+            events :
+            {
+                onClick : function(Item) {
+                    alert( 'click: '+ Item.getAttribute( 'text' ) );
+                }
+            }
+        })
+    );
+
+
+    // open the menu with the context menu
+    document.body.addEvent('contextmenu', function(event)
+    {
+        event.stop();
+
+        Menu.setPosition( event.page.x, event.page.y ).show().focus();
+    });
+});
