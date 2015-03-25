21323b1cc9bf27d5326bfe58690e83049c98cbe7
Henning Leutz <leutz@pcsg.de>
2015-03-25 10:08:15 +0100

    new loader and variable loader

--- qui/controls/loader/Loader.js
+++ qui/controls/loader/Loader.js
@@ -34,12 +34,16 @@ define('qui/controls/loader/Loader', [
         options : {
             cssclass  : '',    // extra CSS class
             closetime : 50000, // seconds if the closing window showed
-            styles    : false  // extra CSS styles
+            styles    : false,  // extra CSS styles,
+            type      : 'standard'
         },
 
         initialize : function(options)
         {
             this.parent( options );
+
+            this.$Inner = null;
+            this.$FX    = null;
             this.$delay = null;
 
             this.addEvent('onDestroy', function()
@@ -48,6 +52,23 @@ define('qui/controls/loader/Loader', [
                     this.$Elm.getParent().removeClass( 'qui-loader-parent' );
                 }
             });
+
+            this.$animations = {
+                standard : {
+                    children : 8,
+                    files    : ['css!qui/controls/loader/Loader.standard.css']
+                },
+
+                'line-scale' : {
+                    children : 5,
+                    files    : ['css!qui/controls/loader/Loader.line-scale.css']
+                },
+
+                'ball-pulse-rise' : {
+                    children : 5,
+                    files    : ['css!qui/controls/loader/Loader.ball-pulse-rise.css']
+                }
+            };
         },
 
         /**
@@ -60,30 +81,24 @@ define('qui/controls/loader/Loader', [
         {
             this.$Elm = new Element('div', {
                 'class' : 'qui-loader',
-                html : '<div class="qui-loader-message"></div>'+
-                       '<div class="qui-loader-bar">' +
-                           '<div class="qui-loader-dot qui-loader-bar1"></div>' +
-                           '<div class="qui-loader-dot qui-loader-bar2"></div>' +
-                           '<div class="qui-loader-dot qui-loader-bar3"></div>' +
-                           '<div class="qui-loader-dot qui-loader-bar4"></div>' +
-                           '<div class="qui-loader-dot qui-loader-bar5"></div>' +
-                           '<div class="qui-loader-dot qui-loader-bar6"></div>' +
-                           '<div class="qui-loader-dot qui-loader-bar7"></div>' +
-                           '<div class="qui-loader-dot qui-loader-bar8"></div>' +
-                       '</div>',
+                html    : '<div class="qui-loader-message"></div>'+
+                          '<div class="qui-loader-inner"></div>',
                 styles : {
                     display : 'none',
                     opacity : 0.8
                 }
             });
 
-            if ( this.getAttribute('cssclass') ) {
-                this.$Elm.addClass( this.getAttribute('cssclass') );
+            if ( this.getAttribute( 'cssclass' ) ) {
+                this.$Elm.addClass( this.getAttribute( 'cssclass' ) );
             }
 
             if ( this.getAttribute( 'styles' ) ) {
                 this.$Elm.setStyles( this.getAttribute( 'styles' ) );
             }
+
+            this.$Inner   = this.$Elm.getElement( '.qui-loader-inner' );
+            this.$Message = this.$Elm.getElement( '.qui-loader-message' );
 
             return this.$Elm;
         },
@@ -103,68 +118,78 @@ define('qui/controls/loader/Loader', [
                 return;
             }
 
-            var Message = this.$Elm.getElement( '.qui-loader-message' ),
-                Bar     = this.$Elm.getElement( '.qui-loader-bar' ),
-                size    = this.$Elm.getSize();
-
-            Message.set( 'html', '' );
-
-            var top  = (size.y - 50) / 2,
-                left = (size.x - 240) / 2;
-
-            if ( top < 0 )
-            {
+            var self = this,
                 size = this.$Elm.measure(function() {
                     return this.getSize();
                 });
 
-                top  = (size.y - 50) / 2;
-                left = (size.x - 240) / 2;
-            }
-
-            Bar.setStyles({
-                top  : top,
-                left : left
-            });
+            this.$Message.set( 'html', '' );
 
             if ( typeof str !== 'undefined' )
             {
-                Message.set({
+                this.$Message.set({
                     html   : str,
                     styles : {
                         top : (size.y + 20) / 2
                     }
                 });
-
             }
 
-            this.$Elm.setStyle( 'display', '' );
+            // load animation
+            var animationData = this.$animations.standard,
+                animationType = 'standard';
 
-            if ( !this.$Elm.getParent().hasClass( 'qui-window-popup' ) ) {
-                this.$Elm.getParent().addClass( 'qui-loader-parent' );
-            }
-
-            if ( !this.getAttribute( 'closetime' ) ) {
-                return;
-            }
-
-            // sicherheitsabfrage nach 10 sekunden
-            if ( this.$delay ) {
-                clearTimeout( this.$delay );
-            }
-
-            this.$delay = (function()
+            if ( this.getAttribute( 'type' ) in this.$animations )
             {
-                this.showCloseButton();
-            }).delay( this.getAttribute( 'closetime' ) , this );
+                animationData = this.$animations[ this.getAttribute( 'type' ) ];
+                animationType = this.getAttribute( 'type' );
+            }
+
+            require( animationData.files , function()
+            {
+                self.$Inner.set( 'html', '' );
+
+                var Parent = new Element('div', {
+                    'class' : 'qui-loader-inner-'+ animationType
+                }).inject( self.$Inner );
+
+                for ( var i = 0, len = animationData.children; i < len; i++ )
+                {
+                    new Element('div', {
+                        'class' : 'control-background'
+                    }).inject( Parent );
+                }
+
+
+                self.$Elm.setStyle( 'display', '' );
+
+                if ( !self.$Elm.getParent().hasClass( 'qui-window-popup' ) ) {
+                    self.$Elm.getParent().addClass( 'qui-loader-parent' );
+                }
+
+
+                if ( !self.getAttribute( 'closetime' ) ) {
+                    return;
+                }
+
+                // sicherheitsabfrage nach 10 sekunden
+                if ( self.$delay ) {
+                    clearTimeout( self.$delay );
+                }
+
+                self.$delay = (function() {
+                    self.showCloseButton();
+                }).delay( self.getAttribute( 'closetime' ) , self );
+            });
         },
 
         /**
          * Hide the loader
          *
          * @method controls/loader/Loader#hide
+         * @param {Function} [callback] - callback function, trigger at animation end
          */
-        hide : function()
+        hide : function(callback)
         {
             if ( this.$delay ) {
                 clearTimeout( this.$delay );
@@ -174,7 +199,18 @@ define('qui/controls/loader/Loader', [
                 return;
             }
 
-            this.$Elm.setStyle( 'display', 'none' );
+            var self = this;
+
+            this.$FX.animate({
+                opacity : 0
+            }, function()
+            {
+                self.$Elm.setStyle( 'display', 'none' );
+
+                if ( typeof callback === 'function' ) {
+                    callback();
+                }
+            });
         },
 
         /**
@@ -192,11 +228,10 @@ define('qui/controls/loader/Loader', [
             this.$Elm.set({
                 html   : '',
                 styles : {
-                    cursor : 'pointer'
+                    cursor  : 'pointer',
+                    opacity : 0.9
                 }
             });
-
-            this.$Elm.setStyle( 'opacity', 0.9 );
 
             var self = this;
 
