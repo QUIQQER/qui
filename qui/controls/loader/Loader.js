85723eb42569fde4f77bf54b2e0a6dfae28e094c
Henning Leutz <leutz@pcsg.de>
2015-08-27 15:45:03 +0200

    Progressbar -> global color -> control-loader-color

--- qui/controls/loader/Loader.js
+++ qui/controls/loader/Loader.js
@@ -1,4 +1,3 @@
-
 /**
  * A loader control
  * Creates a div with a loader animation
@@ -22,8 +21,7 @@ define('qui/controls/loader/Loader', [
     'qui/Locale',
     'css!qui/controls/loader/Loader.css'
 
-], function(QUI, QUIControl, QUILocale)
-{
+], function (QUI, QUIControl, QUILocale) {
     "use strict";
 
     /**
@@ -33,20 +31,19 @@ define('qui/controls/loader/Loader', [
      */
     return new Class({
 
-        Extends : QUIControl,
-        Type    : 'qui/controls/loader/Loader',
+        Extends: QUIControl,
+        Type   : 'qui/controls/loader/Loader',
 
-        options : {
-            cssclass  : '',     // extra CSS class
-            closetime : 50000,  // seconds if the closing window showed
-            styles    : false,  // extra CSS styles,
-            type      : false,
-            color     : false
+        options: {
+            cssclass : '',     // extra CSS class
+            closetime: 50000,  // seconds if the closing window showed
+            styles   : false,  // extra CSS styles,
+            type     : false,
+            color    : false
         },
 
-        initialize : function(options)
-        {
-            this.parent( options );
+        initialize: function (options) {
+            this.parent(options);
 
             this.$Inner  = null;
             this.$Close  = null;
@@ -54,42 +51,41 @@ define('qui/controls/loader/Loader', [
             this.$delay  = null;
             this.$status = 0;
 
-            this.addEvent('onDestroy', function()
-            {
-                if ( this.$Elm.getParent() ) {
-                    this.$Elm.getParent().removeClass( 'qui-loader-parent' );
+            this.addEvent('onDestroy', function () {
+                if (this.$Elm.getParent()) {
+                    this.$Elm.getParent().removeClass('qui-loader-parent');
                 }
             });
 
             this.$animations = {
-                standard : {
-                    children : 8,
-                    files    : ['css!qui/controls/loader/Loader.standard.css']
+                standard: {
+                    children: 8,
+                    files   : ['css!qui/controls/loader/Loader.standard.css']
                 },
 
-                'line-scale' : {
-                    children : 5,
-                    files    : ['css!qui/controls/loader/Loader.line-scale.css']
+                'line-scale': {
+                    children: 5,
+                    files   : ['css!qui/controls/loader/Loader.line-scale.css']
                 },
 
-                'ball-clip-rotate' : {
-                    children : 1,
-                    files    : ['css!qui/controls/loader/Loader.ball-clip-rotate.css']
+                'ball-clip-rotate': {
+                    children: 1,
+                    files   : ['css!qui/controls/loader/Loader.ball-clip-rotate.css']
                 },
 
-                'ball-pulse-rise' : {
-                    children : 5,
-                    files    : ['css!qui/controls/loader/Loader.ball-pulse-rise.css']
+                'ball-pulse-rise': {
+                    children: 5,
+                    files   : ['css!qui/controls/loader/Loader.ball-pulse-rise.css']
                 },
 
-                'ball-triangle-path' : {
-                    children : 3,
-                    files    : ['css!qui/controls/loader/Loader.ball-triangle-path.css']
+                'ball-triangle-path': {
+                    children: 3,
+                    files   : ['css!qui/controls/loader/Loader.ball-triangle-path.css']
                 },
 
-                'pacman' : {
-                    children : 5,
-                    files    : ['css!qui/controls/loader/Loader.pacman.css']
+                'pacman': {
+                    children: 5,
+                    files   : ['css!qui/controls/loader/Loader.pacman.css']
                 }
             };
         },
@@ -100,30 +96,29 @@ define('qui/controls/loader/Loader', [
          * @method controls/loader/Loader#create
          * @return {HTMLElement}
          */
-        create : function()
-        {
+        create: function () {
             this.$Elm = new Element('div', {
-                'class' : 'qui-loader',
-                html    : '<div class="qui-loader-message"></div>'+
-                          '<div class="qui-loader-inner"></div>',
+                'class': 'qui-loader',
+                html   : '<div class="qui-loader-message"></div>' +
+                         '<div class="qui-loader-inner"></div>',
                 styles : {
-                    display : 'none',
-                    opacity : 0.8
+                    display: 'none',
+                    opacity: 0.8
                 }
             });
 
-            if ( this.getAttribute( 'cssclass' ) ) {
-                this.$Elm.addClass( this.getAttribute( 'cssclass' ) );
+            if (this.getAttribute('cssclass')) {
+                this.$Elm.addClass(this.getAttribute('cssclass'));
             }
 
-            if ( this.getAttribute( 'styles' ) ) {
-                this.$Elm.setStyles( this.getAttribute( 'styles' ) );
+            if (this.getAttribute('styles')) {
+                this.$Elm.setStyles(this.getAttribute('styles'));
             }
 
-            this.$Inner   = this.$Elm.getElement( '.qui-loader-inner' );
-            this.$Message = this.$Elm.getElement( '.qui-loader-message' );
+            this.$Inner   = this.$Elm.getElement('.qui-loader-inner');
+            this.$Message = this.$Elm.getElement('.qui-loader-message');
 
-            this.$FX = moofx( this.$Elm );
+            this.$FX = moofx(this.$Elm);
 
             return this.$Elm;
         },
@@ -133,20 +128,18 @@ define('qui/controls/loader/Loader', [
          *
          * @method controls/loader/Loader#show
          */
-        show : function(str)
-        {
+        show: function (str) {
             this.$status = 1;
 
-            if ( !this.$Elm ) {
+            if (!this.$Elm) {
                 return;
             }
 
-            if ( !this.$Elm.getParent() ) {
+            if (!this.$Elm.getParent()) {
                 return;
             }
 
-            if ( this.$Close )
-            {
+            if (this.$Close) {
                 this.$Close.destroy();
                 this.$Close = null;
 
@@ -155,32 +148,31 @@ define('qui/controls/loader/Loader', [
                           '<div class="qui-loader-inner"></div>'
                 });
 
-                this.$Inner   = this.$Elm.getElement( '.qui-loader-inner' );
-                this.$Message = this.$Elm.getElement( '.qui-loader-message' );
+                this.$Inner   = this.$Elm.getElement('.qui-loader-inner');
+                this.$Message = this.$Elm.getElement('.qui-loader-message');
             }
 
             var self = this,
-                size = this.$Elm.measure(function() {
+                size = this.$Elm.measure(function () {
                     return this.getSize();
                 });
 
-            this.$Message.set( 'html', '' );
+            this.$Message.set('html', '');
 
-            if ( typeof str !== 'undefined' )
-            {
+            if (typeof str !== 'undefined') {
                 this.$Message.set({
-                    html   : str,
-                    styles : {
-                        top : (size.y + 20) / 2
+                    html  : str,
+                    styles: {
+                        top: (size.y + 20) / 2
                     }
                 });
             }
 
             // must be showed, because, hide can be triggered -> no async showed
-            this.$Elm.setStyle( 'display', '' );
+            this.$Elm.setStyle('display', '');
 
             this.$FX.animate({
-                opacity : 0.8
+                opacity: 0.8
             });
 
 
@@ -188,84 +180,77 @@ define('qui/controls/loader/Loader', [
             var animationData = false,
                 animationType = false;
 
-            if ( this.getAttribute( 'type' ) &&
-                 this.getAttribute( 'type' ) in this.$animations )
-            {
-                animationData = this.$animations[ this.getAttribute( 'type' ) ];
-                animationType = this.getAttribute( 'type' );
+            if (this.getAttribute('type') &&
+                this.getAttribute('type') in this.$animations) {
+                animationData = this.$animations[this.getAttribute('type')];
+                animationType = this.getAttribute('type');
             }
 
-            if ( !animationType &&
-                 QUI.getAttribute( 'control-loader-type' ) &&
-                 QUI.getAttribute( 'control-loader-type' ) in this.$animations )
-            {
-                animationData = this.$animations[ QUI.getAttribute( 'control-loader-type' ) ];
-                animationType = QUI.getAttribute( 'control-loader-type' );
+            if (!animationType &&
+                QUI.getAttribute('control-loader-type') &&
+                QUI.getAttribute('control-loader-type') in this.$animations) {
+                animationData = this.$animations[QUI.getAttribute('control-loader-type')];
+                animationType = QUI.getAttribute('control-loader-type');
             }
 
-            if ( !animationType )
-            {
+            if (!animationType) {
                 animationData = this.$animations.standard;
                 animationType = 'standard';
             }
 
-            require( animationData.files , function()
-            {
-                if ( self.$status === 0 )
-                {
+            require(animationData.files, function () {
+                if (self.$status === 0) {
                     self.hide();
                     return;
                 }
 
-                self.$Inner.set( 'html', '' );
+                self.$Inner.set('html', '');
 
                 var i, len, Child;
 
                 var Parent = new Element('div', {
-                    'class' : 'qui-loader-inner-'+ animationType
-                }).inject( self.$Inner );
+                    'class': 'qui-loader-inner-' + animationType
+                }).inject(self.$Inner);
 
-                var color = self.getAttribute( 'color' );
+                var color = self.getAttribute('color');
 
-                if ( !color ) {
-                    color = QUI.getAttribute( 'control-loader-color' );
+                if (!color) {
+                    color = QUI.getAttribute('control-loader-color');
                 }
 
-                for ( i = 0, len = animationData.children; i < len; i++ )
-                {
+                for (i = 0, len = animationData.children; i < len; i++) {
                     Child = new Element('div', {
-                        'class' : 'control-background'
-                    }).inject( Parent );
+                        'class': 'control-background'
+                    }).inject(Parent);
 
-                    if ( color ) {
-                        Child.setStyle( 'background', color );
+                    if (color) {
+                        Child.setStyle('background', color);
                     }
                 }
 
                 var ElmParent = self.$Elm.getParent();
 
-                if ( ElmParent && !ElmParent.hasClass( 'qui-window-popup' ) ) {
-                    ElmParent.addClass( 'qui-loader-parent' );
+                if (ElmParent && !ElmParent.hasClass('qui-window-popup')) {
+                    ElmParent.addClass('qui-loader-parent');
                 }
 
-                if ( self.$status === 0 )
-                {
+                if (self.$status === 0) {
                     self.hide();
                     return;
                 }
 
-                if ( !self.getAttribute( 'closetime' ) ) {
+                if (!self.getAttribute('closetime')) {
                     return;
                 }
 
                 // sicherheitsabfrage nach 10 sekunden
-                if ( self.$delay ) {
-                    clearTimeout( self.$delay );
+                if (self.$delay) {
+                    clearTimeout(self.$delay);
                 }
 
-                self.$delay = (function() {
+                self.$delay = (function () {
                     self.showCloseButton();
-                }).delay( self.getAttribute( 'closetime' ) , self );
+                }).delay(self.getAttribute('closetime'), self);
             });
         },
 
@@ -275,23 +260,21 @@ define('qui/controls/loader/Loader', [
          * @method controls/loader/Loader#hide
          * @param {Function} [callback] - callback function, trigger at animation end
          */
-        hide : function(callback)
-        {
+        hide: function (callback) {
             this.$status = 0;
 
-            if ( this.$delay ) {
-                clearTimeout( this.$delay );
+            if (this.$delay) {
+                clearTimeout(this.$delay);
             }
 
-            if ( !this.$Elm ) {
+            if (!this.$Elm) {
                 return;
             }
 
-            if ( !this.$FX )
-            {
-                this.$Elm.setStyle( 'display', 'none' );
+            if (!this.$FX) {
+                this.$Elm.setStyle('display', 'none');
 
-                if ( typeof callback === 'function' ) {
+                if (typeof callback === 'function') {
                     callback();
                 }
 
@@ -301,13 +284,12 @@ define('qui/controls/loader/Loader', [
             var self = this;
 
             this.$FX.animate({
-                opacity : 0
-            }, function()
-            {
-                self.$Elm.setStyle( 'display', 'none' );
+                opacity: 0
+            }, function () {
+                self.$Elm.setStyle('display', 'none');
                 self.$status = 0;
 
-                if ( typeof callback === 'function' ) {
+                if (typeof callback === 'function') {
                     callback();
                 }
             });
@@ -319,36 +301,34 @@ define('qui/controls/loader/Loader', [
          *
          * @method controls/loader/Loader#showCloseButton
          */
-        showCloseButton : function()
-        {
-            if ( !this.$Elm ) {
+        showCloseButton: function () {
+            if (!this.$Elm) {
                 return;
             }
 
             this.$Elm.set({
-                html   : '',
-                styles : {
-                    cursor  : 'pointer',
-                    opacity : 0.9
+                html  : '',
+                styles: {
+                    cursor : 'pointer',
+                    opacity: 0.9
                 }
             });
 
             var self = this;
 
             this.$Close = new Element('div', {
-                text   : QUILocale.get( 'quiqqer/controls', 'loader.close' ),
-                styles : {
-                    'font-weight' : 'bold',
-                    'text-align'  : 'center',
-                    'margin-top'  : (this.$Elm.getSize().y / 2) - 100
+                text  : QUILocale.get('quiqqer/controls', 'loader.close'),
+                styles: {
+                    'font-weight': 'bold',
+                    'text-align' : 'center',
+                    'margin-top' : (this.$Elm.getSize().y / 2) - 100
                 },
-                events :
-                {
-                    click : function() {
+                events: {
+                    click: function () {
                         self.hide();
                     }
                 }
-            }).inject( this.$Elm );
+            }).inject(this.$Elm);
         }
     });
 });
--- qui/controls/loader/Progress.js
+++ qui/controls/loader/Progress.js
@@ -1,4 +1,3 @@
-
 /**
  * A progress bar control
  * Creates a div with a progress animation
@@ -7,17 +6,19 @@
  * @author www.pcsg.de (Henning Leutz)
  *
  * @require qui/controls/Control
- * @require qui/Locale
  * @require css!qui/controls/loader/Progress.css
+ *
+ * Global QUI Attribute
+ * - control-loader-color
  */
 
 define('qui/controls/loader/Progress', [
 
+    'qui/QUI',
     'qui/controls/Control',
     'css!qui/controls/loader/Progress.css'
 
-], function(QUIControl)
-{
+], function (QUI, QUIControl) {
     "use strict";
 
     /**
@@ -27,22 +28,28 @@ define('qui/controls/loader/Progress', [
      */
     return new Class({
 
-        Extends : QUIControl,
-        Type    : 'qui/controls/loader/Progress',
+        Extends: QUIControl,
+        Type   : 'qui/controls/loader/Progress',
 
         options: {
-            color  : '#d52349',
-            styles : false
+            color : '#d52349',
+            styles: false
         },
 
-        initialize: function (options)
-        {
-            this.parent( options );
+        initialize: function (options) {
+
+            // default global color
+            if (QUI.getAttribute('control-loader-color')) {
+                this.setAttribute(
+                    'color',
+                    QUI.getAttribute('control-loader-color')
+                );
+            }
+
+            this.parent(options);
 
             this.$Bar   = null;
             this.$BarFX = null;
-
-            this.$interval = false;
         },
 
         /**
@@ -51,26 +58,25 @@ define('qui/controls/loader/Progress', [
          * @method controls/loader/Progress#create
          * @return {HTMLElement}
          */
-        create: function ()
-        {
-            var color = this.getAttribute( 'color' );
+        create: function () {
+            var color = this.getAttribute('color');
 
             this.$Elm = this.parent();
-            this.$Elm.addClass( 'qui-progress' );
+            this.$Elm.addClass('qui-progress');
 
             this.$Bar = new Element('div', {
-                'class' : 'qui-progress-bar',
-                styles  : {
+                'class': 'qui-progress-bar',
+                styles : {
                     background: color,
-                    boxShadow : '0 0 10px '+ color +', 0 0 5px '+ color
+                    boxShadow : '0 0 10px ' + color + ', 0 0 5px ' + color
                 }
-            }).inject( this.$Elm );
+            }).inject(this.$Elm);
 
-            this.$BarFX = moofx( this.$Bar );
+            this.$BarFX = moofx(this.$Bar);
             this.reset();
 
-            if ( this.getAttribute( 'styles' ) ) {
-                this.$Bar.setStyles( this.getAttribute( 'styles' ) );
+            if (this.getAttribute('styles')) {
+                this.$Bar.setStyles(this.getAttribute('styles'));
             }
 
             return this.$Elm;
@@ -79,10 +85,9 @@ define('qui/controls/loader/Progress', [
         /**
          * reset the bar
          */
-        reset : function()
-        {
+        reset: function () {
             this.stopIncrement();
-            this.$BarFX.style( 'width', 0 );
+            this.$BarFX.style('width', 0);
         },
 
         /**
@@ -90,13 +95,12 @@ define('qui/controls/loader/Progress', [
          *
          * @param {Number} step - percentage step (0 - 100)
          */
-        set : function(step)
-        {
+        set: function (step) {
             var width   = this.$Elm.getSize().x,
                 newSize = (( width / 100 ) * step).round();
 
             this.$BarFX.animate({
-                width : newSize
+                width: newSize
             });
         },
 
@@ -106,24 +110,22 @@ define('qui/controls/loader/Progress', [
          *
          * @param {Number} ms - miliseconds to finish
          */
-        increment : function(ms)
-        {
+        increment: function (ms) {
             this.reset();
 
             this.$BarFX.animate({
-                width : '100%'
+                width: '100%'
             }, {
-                duration : ms,
-                equation : 'linear'
+                duration: ms,
+                equation: 'linear'
             });
         },
 
         /**
          * stop the increment
          */
-        stopIncrement : function()
-        {
-            this.$BarFX.style( 'width', this.$BarFX.compute( 'width' ) );
+        stopIncrement: function () {
+            this.$BarFX.style('width', this.$BarFX.compute('width'));
         }
     });
 });
