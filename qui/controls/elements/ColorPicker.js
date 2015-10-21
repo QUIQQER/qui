09816af16b663fc9a0a22f6e44cba48f60521bc5
Henning Leutz <leutz@pcsg.de>
2015-10-21 11:09:54 +0200

    New Control -> ColorPicker

---
+++ qui/controls/elements/ColorPicker.js
@@ -0,0 +1,168 @@
+/**
+ * Color Picker
+ * only html5 compatible
+ *
+ * @module qui/controls/elements/ColorPicker
+ * @author www.pcsg.de (Henning Leutz)
+ *
+ * @require qui/QUI
+ * @require qui/controls/Control
+ * @require qui/controls/elements/ColorPicker.css
+ */
+define('qui/controls/elements/ColorPicker', [
+
+    'qui/QUI',
+    'qui/controls/Control',
+    'qui/controls/buttons/Button',
+
+    'css!qui/controls/elements/ColorPicker.css'
+
+], function (QUI, QUIControl, QUIButton) {
+    "use strict";
+
+    return new Class({
+
+        Extends: QUIControl,
+        Type   : 'qui/controls/elements/ColorPicker',
+
+        Binds: [
+            'clear',
+            '$onImport'
+        ],
+
+        initialize: function (options) {
+
+            this.parent(options);
+
+            this.$Input          = null;
+            this.$Color          = null;
+            this.$ColorContainer = null;
+            this.$Clear          = null;
+
+            this.addEvents({
+                onImport: this.$onImport
+            });
+        },
+
+        /**
+         * Create the DOMNode Element
+         * @returns {HTMLDivElement}
+         */
+        create: function () {
+            var self = this;
+
+            this.$Elm = new Element('div', {
+                'class': 'qui-controls-colorpicker',
+                html   : '<div class="qui-controls-colorpicker-colorContainer"></div>'
+            });
+
+            if (!this.$Color) {
+                this.$Color = new Element('input', {
+                    type: 'color'
+                }).inject(this.$Elm);
+            }
+
+            this.$Input = new Element('input', {
+                name: '',
+                type: 'hidden'
+            }).inject(this.$Elm);
+
+            this.$Clear = new QUIButton({
+                icon  : 'fa fa-close icon-remove',
+                events: {
+                    onClick: this.clear
+                }
+            }).inject(this.$Elm);
+
+            this.$Clear.getElm().addClass(
+                'qui-controls-colorpicker-clearButton'
+            );
+
+            this.$ColorContainer = this.$Elm.getElement(
+                '.qui-controls-colorpicker-colorContainer'
+            );
+
+            this.$Color.addEvent('change', function () {
+                self.setValue(self.$Color.value);
+            });
+
+            this.$Color.setStyles({
+                display: 'none'
+            });
+
+            this.$ColorContainer.addEvent('click', function () {
+                self.$Color.click();
+            });
+
+            return this.$Elm;
+        },
+
+        /**
+         * event : on import
+         */
+        $onImport: function () {
+            this.$Color = this.getElm();
+
+            var Parent = this.create();
+            Parent.wraps(this.$Color);
+
+            this.$Clear.inject(this.$Color, 'after');
+
+            this.$Input.set({
+                name: this.$Color.get('name'),
+                id  : this.$Color.get('id')
+            });
+
+            this.$Color.set({
+                name: null,
+                id  : null
+            });
+
+            var realValue = this.$Color.get('data-realvalue');
+
+            if (realValue === '') {
+                this.clear();
+                return;
+            }
+
+            this.setValue(this.$Color.get('value'));
+        },
+
+        /**
+         * Clears the value
+         */
+        clear: function () {
+            this.$Input.value = '';
+            this.$Color.value = '';
+
+            this.$ColorContainer.setStyle('backgroundColor', null);
+        },
+
+        /**
+         * Set the color
+         *
+         * @param {String} color
+         */
+        setValue: function (color) {
+
+            if (color === '') {
+                this.clear();
+                return;
+            }
+
+            this.$Color.value = color;
+            this.$Input.value = this.$Color.value;
+
+            this.$ColorContainer.setStyle('backgroundColor', this.$Color.value);
+        },
+
+        /**
+         * Return the value
+         *
+         * @returns {String}
+         */
+        getValue: function () {
+            return this.$Input.value;
+        }
+    });
+});
--- qui/utils/Form.js
+++ qui/utils/Form.js
@@ -1,4 +1,3 @@
-
 /**
  * Helper for <form> nodes
  *
@@ -17,11 +16,10 @@ define('qui/utils/Form', {
      * @param {Object} data
      * @param {HTMLElement} form - Formular
      */
-    setDataToForm : function(data, form)
-    {
+    setDataToForm: function (data, form) {
         "use strict";
 
-        if ( typeof form === 'undefined' || form.nodeName !== 'FORM' ) {
+        if (typeof form === 'undefined' || form.nodeName !== 'FORM') {
             return;
         }
 
@@ -30,44 +28,40 @@ define('qui/utils/Form', {
         data = data || {};
 
         // unselect checkboxes and radios
-        for ( k in data )
-        {
-            if ( !data.hasOwnProperty( k ) ) {
+        for (k in data) {
+            if (!data.hasOwnProperty(k)) {
                 continue;
             }
 
-            if ( typeof form.elements[ k ] === 'undefined' ) {
+            if (typeof form.elements[k] === 'undefined') {
                 continue;
             }
 
-            Elm = form.elements[ k ];
+            Elm = form.elements[k];
 
-            if ( Elm.type === 'checkbox' || Elm.type === 'radio' ) {
-                form.getElements( '[name="'+ k +'"]' ).set( 'checked', false );
+            if (Elm.type === 'checkbox' || Elm.type === 'radio') {
+                form.getElements('[name="' + k + '"]').set('checked', false);
             }
 
-            if ( Elm.length && ( Elm[0].type === 'checkbox' || Elm[0].type === 'radio' ) ) {
-                form.getElements( '[name="'+ k +'"]' ).set( 'checked', false );
+            if (Elm.length && ( Elm[0].type === 'checkbox' || Elm[0].type === 'radio' )) {
+                form.getElements('[name="' + k + '"]').set('checked', false);
             }
         }
 
-        for ( k in data )
-        {
-            if ( !data.hasOwnProperty( k ) ) {
+        for (k in data) {
+            if (!data.hasOwnProperty(k)) {
                 continue;
             }
 
-            if ( typeof form.elements[ k ] === 'undefined' ) {
+            if (typeof form.elements[k] === 'undefined') {
                 continue;
             }
 
 
-            Elm = form.elements[ k ];
+            Elm = form.elements[k];
 
-            if ( Elm.type === 'checkbox' )
-            {
-                if ( data[k] === false || data[k] === true )
-                {
+            if (Elm.type === 'checkbox') {
+                if (data[k] === false || data[k] === true) {
                     Elm.checked = data[k];
                     continue;
                 }
@@ -76,12 +70,12 @@ define('qui/utils/Form', {
                 continue;
             }
 
-            if ( Elm.type === 'text' ||
-                 Elm.type === 'hidden' ||
-                 Elm.nodeName === 'TEXTAREA' ||
-                 Elm.nodeName === 'SELECT' )
-            {
-                if ( typeOf( data[k] ) == 'boolean' ) {
+            if (Elm.type === 'text' ||
+                Elm.type === 'hidden' ||
+                Elm.nodeName === 'TEXTAREA' ||
+                Elm.nodeName === 'SELECT') {
+
+                if (typeOf(data[k]) == 'boolean') {
                     continue;
                 }
 
@@ -89,26 +83,23 @@ define('qui/utils/Form', {
                 continue;
             }
 
-            if ( Elm.length )
-            {
-                for ( i = 0, len = Elm.length; i < len; i++ )
-                {
-                    if ( Elm[ i ].type !== 'radio' && Elm[ i ].type !== 'checkbox' ) {
+            if (Elm.length) {
+                for (i = 0, len = Elm.length; i < len; i++) {
+                    if (Elm[i].type !== 'radio' && Elm[i].type !== 'checkbox') {
                         continue;
                     }
 
-                    val = Elm[ i ].value;
+                    val = Elm[i].value;
 
-                    if ( typeOf( data[ k ] ) == 'array' )
-                    {
-                        if ( data[ k ].contains( val ) ) {
+                    if (typeOf(data[k]) == 'array') {
+                        if (data[k].contains(val)) {
                             Elm[i].checked = true;
                         }
 
                         continue;
                     }
 
-                    if ( val == data[ k ] ) {
+                    if (val == data[k]) {
                         Elm[i].checked = true;
                     }
                 }
@@ -116,7 +107,11 @@ define('qui/utils/Form', {
                 continue;
             }
 
-            Elm.value = data[ k ];
+            if (Elm.type === 'color') {
+                Elm.set('data-realvalue', data[k]);
+            }
+
+            Elm.value = data[k];
         }
     },
 
@@ -128,11 +123,10 @@ define('qui/utils/Form', {
      * @param {HTMLElement} form - DOMNode Formular
      * @return {Object}
      */
-    getFormData : function(form)
-    {
+    getFormData: function (form) {
         "use strict";
 
-        if ( typeof form === 'undefined' || !form ) {
+        if (typeof form === 'undefined' || !form) {
             return {};
         }
 
@@ -141,8 +135,7 @@ define('qui/utils/Form', {
         var result   = {},
             elements = form.elements;
 
-        for ( i = 0, len = elements.length; i < len; i++ )
-        {
+        for (i = 0, len = elements.length; i < len; i++) {
             Elm = elements[i];
             n   = Elm.name;
 
@@ -150,52 +143,47 @@ define('qui/utils/Form', {
                 continue;
             }
 
-            if ( Elm.type === 'checkbox' )
-            {
+            if (Elm.type === 'checkbox') {
                 // array
-                if ( elements[ n ].length )
-                {
-                    if ( typeof result[ Elm.name ] === 'undefined' ) {
-                        result[ n ] = [];
+                if (elements[n].length) {
+                    if (typeof result[Elm.name] === 'undefined') {
+                        result[n] = [];
                     }
 
-                    if ( Elm.checked ) {
-                        result[ n ].push( Elm.value );
+                    if (Elm.checked) {
+                        result[n].push(Elm.value);
                     }
 
                     continue;
                 }
 
-                result[ n ] = Elm.checked ? true : false;
+                result[n] = Elm.checked ? true : false;
                 continue;
             }
 
-            if ( Elm.type === 'radio' && !Elm.length )
-            {
-                if ( Elm.checked ) {
-                    result[ n ] = Elm.value;
+            if (Elm.type === 'radio' && !Elm.length) {
+                if (Elm.checked) {
+                    result[n] = Elm.value;
                 }
 
                 continue;
             }
 
-            if ( Elm.type === 'radio' && Elm.length )
-            {
-                for ( i = 0, len = Elm.length; i < len; i++ )
-                {
-                    if ( Elm[i].type !== 'radio' ) {
+            if (Elm.type === 'radio' && Elm.length) {
+                for (i = 0, len = Elm.length; i < len; i++) {
+                    if (Elm[i].type !== 'radio') {
                         continue;
                     }
 
-                    result[ Elm[i].name ] = '';
+                    result[Elm[i].name] = '';
 
-                    if ( Elm[i].checked ) {
-                        result[ Elm[i].name ] = Elm[i].value;
+                    if (Elm[i].checked) {
+                        result[Elm[i].name] = Elm[i].value;
                     }
                 }
             }
 
-            result[ n ] = Elm.value;
+            result[n] = Elm.value;
         }
 
         return result;
@@ -209,26 +197,25 @@ define('qui/utils/Form', {
      * @param {HTMLElement} el
      * @param {String} text
      */
-    insertTextAtCursor : function(el, text)
-    {
+    insertTextAtCursor: function (el, text) {
         "use strict";
 
         var val = el.value, endIndex, range;
 
-        if ( typeof el.selectionStart != "undefined" &&
-             typeof el.selectionEnd != "undefined")
-        {
-            endIndex = el.selectionEnd;
-            el.value = val.slice(0, el.selectionStart) + text + val.slice(endIndex);
+        if (typeof el.selectionStart != "undefined" &&
+            typeof el.selectionEnd != "undefined") {
+
+            endIndex          = el.selectionEnd;
+            el.value          = val.slice(0, el.selectionStart) + text + val.slice(endIndex);
             el.selectionStart = el.selectionEnd = endIndex + text.length;
 
-        } else if ( typeof document.selection != "undefined" &&
-                    typeof document.selection.createRange != "undefined")
-        {
+        } else if (typeof document.selection != "undefined" &&
+                   typeof document.selection.createRange != "undefined") {
+
             el.focus();
 
-            range = document.selection.createRange();
-            range.collapse( false );
+            range      = document.selection.createRange();
+            range.collapse(false);
             range.text = text;
             range.select();
         }
