d80b9203eceed34a9aaf933657c656d02ed4205c
Henning Leutz <leutz@pcsg.de>
2016-01-16 08:51:59 +0100

    quiqqer/quiqqer#367 -> new method formatBytes

--- qui/utils/String.js
+++ qui/utils/String.js
@@ -1,4 +1,3 @@
-
 /**
  * string helper
  * Helps with string operations
@@ -6,7 +5,6 @@
  * @module qui/utils/String
  * @author www.pcsg.de (Henning Leutz)
  */
-
 define('qui/utils/String', {
 
     /**
@@ -17,13 +15,12 @@ define('qui/utils/String', {
      * @param {String} str - index.php?param1=12&param2=test
      * @return {Object}
      */
-    getUrlParams : function(str)
-    {
+    getUrlParams: function (str) {
         "use strict";
 
         var params = str.split('?');
 
-        if ( typeof params[1] === 'undefined' ){
+        if (typeof params[1] === 'undefined') {
             return {};
         }
 
@@ -32,13 +29,34 @@ define('qui/utils/String', {
         var i, len, sp;
         var r = {};
 
-        for ( i = 0, len = params.length; i < len; i++ )
-        {
+        for (i = 0, len = params.length; i < len; i++) {
             sp = params[i].split('=');
 
-            r[ sp[0] ] = sp[1];
+            r[sp[0]] = sp[1];
         }
 
         return r;
+    },
+
+    /**
+     * Format bytes in readable formats
+     *
+     * @param bytes
+     * @param decimals
+     * @returns {String}
+     */
+    formatBytes: function (bytes, decimals) {
+        "use strict";
+
+        if (bytes === 0) {
+            return '0 Byte';
+        }
+
+        var k     = 1000;
+        var dm    = decimals + 1 || 3;
+        var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+        var i     = Math.floor(Math.log(bytes) / Math.log(k));
+
+        return (bytes / Math.pow(k, i)).toPrecision(dm) + ' ' + sizes[i];
     }
 });
